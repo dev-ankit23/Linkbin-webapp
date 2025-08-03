d0a53cd407a54f6d01103a6c5644df1b2468a0a0
devankitx <126304960+devankitx@users.noreply.github.com>
2025-08-03 13:52:52 +0530

    saving hashed password in DB

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { connectDB, UserModel } from "./db.js";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const app = express();
 app.use(express.json());
@@ -21,10 +22,11 @@ app.post("/api/v1/signup", async (req, res) => {
           "User already exists. Please login or choose another username.",
       });
     }
+    const hashedPassword = await bcrypt.hash(password, 10);
 
     const NewUser = await UserModel.create({
       username: username,
-      password: password,
+      password: hashedPassword,
     });
 
     res.status(201).json({
@@ -42,7 +44,8 @@ app.post("/api/v1/signin", async (req, res) => {
   const { username, password } = req.body;
 
   try {
-    const existingUser = await UserModel.findOne({ username, password });
+    // 1. Find the user by username
+    const existingUser = await UserModel.findOne({ username });
 
     if (!existingUser) {
       return res.status(401).json({
@@ -50,7 +53,21 @@ app.post("/api/v1/signin", async (req, res) => {
       });
     }
 
+    // 2. Compare entered password with the hashed password stored in DB
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
+
+    if (!isPasswordCorrect) {
+      return res.status(401).json({
+        message: "User does not exist or wrong credentials",
+      });
+    }
+
+    // 3. If password is correct, generate token
     const token = jwt.sign({ username }, jwtSecret);
+
     return res.status(200).json({
       token,
       message: "Signin successful",
